d52bcf696aa2a52afdc420ed9bcd436b0981197e
kaito-c <kaito1013kaito@gmail.com>
2023-03-21 11:29:21 +0900

    first commit

---
+++ Atcoder/other/東京海上日動プログラミングコンテスト2020/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int a,v,b,w,t;
+    cin >> a >> v >> b >> w >> t;
+    if(v-w>0 && abs(b-a)<=(ll)(v-w)*t){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
