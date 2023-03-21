1fb592a25e62e454e3db4c6772d131463e371b13
kaito-c <kaito1013kaito@gmail.com>
2023-03-21 12:57:52 +0900

    first commit

---
+++ Atcoder/アルゴリズムと数学/005.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int n;
+    cin >> n;
+    int num=0;
+    rep(i,n){
+        int a;
+        cin >> a;
+        num+=a;
+    }
+    cout << num%100 << endl;
+    return 0;
+}
