3da0e071e6b39357dbeed31580d9ef18300b6b54
kaito-c <kaito1013kaito@gmail.com>
2023-03-21 23:01:11 +0900

    first commit

--- Atcoder/競技プログラミングの鉄則/A13.cpp
+++ Atcoder/競技プログラミングの鉄則/A13.cpp
@@ -13,6 +13,24 @@ vector<int>dys={0,1,0,-1};
 //fixed << setprecision(10)
 
 int main(){
-    
+    int n,k;
+    cin >> n >> k;
+    vector<int>A(n+1),R(n+1);
+    for(int i=1;i<=n;i++)cin >> A[i];
+
+    for(int i=1;i<n;i++){
+        if(i==1)R[i]=1;
+        else R[i]=R[i-1];
+
+        while(R[i]<n && A[R[i]+1]-A[i]<=k){
+            R[i]++;
+        }
+    }
+
+    ll ans=0;
+    for(int i=1;i<n;i++){
+        ans+=R[i]-i;
+    }
+    cout << ans << endl;
     return 0;
 }
