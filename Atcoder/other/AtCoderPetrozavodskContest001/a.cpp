aa348778bee554f35adda3f7de92d52cddbdbf73
kaito-c <kaito1013kaito@gmail.com>
2023-03-23 12:12:44 +0900

    first commit

---
+++ Atcoder/other/AtCoderPetrozavodskContest001/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int x,y;
+    cin >> x >> y;
+    if(x%y==0){
+        cout << -1 << endl;
+    }else{
+        cout << x << endl;
+    }
+    return 0;
+}
---
+++ Atcoder/other/キーエンスプログラミングコンテスト2020/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int h,w,n;
+    cin >> h >> w >> n;
+    cout << (n+max(h,w)-1)/max(h,w) << endl;
+    return 0;
+}
