cd5be7a7a9084fa7520598934dc4f29e6b02da5d
kaito-c <kaito1013kaito@gmail.com>
2023-03-25 09:40:25 +0900

    first commit

--- Atcoder/競技プログラミングの鉄則/A14.cpp
+++ Atcoder/競技プログラミングの鉄則/A14.cpp
@@ -13,6 +13,26 @@ vector<int>dys={0,1,0,-1};
 //fixed << setprecision(10)
 
 int main(){
-    
+    int n,k;
+    cin >> n >> k;
+    vector<int>A(n),B(n),C(n),D(n);
+    rep(i,n)cin >> A[i];
+    rep(i,n)cin >> B[i];
+    rep(i,n)cin >> C[i];
+    rep(i,n)cin >> D[i];
+    vector<int>P,Q;
+    rep(i,n)rep(j,n)P.push_back(A[i]+B[j]);
+    rep(i,n)rep(j,n)Q.push_back(C[i]+D[j]);
+
+    sort(ALL(Q));
+
+    rep(i,n*n){
+        int pos=lower_bound(ALL(Q),k-P[i])-Q.begin();
+        if(pos<Q.size() && Q[pos]==k-P[i]){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
