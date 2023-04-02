eed9a4629905a6a1369c2109278ec32e26174c26
kaito-c <kaito1013kaito@gmail.com>
2023-04-02 22:28:51 +0900

    first commit

---
+++ Atcoder/ARC/ARC009/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int n;
+    cin >> n;
+    ll ans=0;
+    rep(i,n){
+        int a,b;
+        cin >> a >> b;
+        ans+=a*b;
+    }
+    cout << ans*105/100 << endl;
+    return 0;
+}
