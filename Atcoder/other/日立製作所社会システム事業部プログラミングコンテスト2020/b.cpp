a114d63039f57f948a99a01e7bea576c096f64bd
kaito-c <kaito1013kaito@gmail.com>
2023-04-05 21:00:39 +0900

    first commit

--- Atcoder/other/日立製作所社会システム事業部プログラミングコンテスト2020/b.cpp
+++ Atcoder/other/日立製作所社会システム事業部プログラミングコンテスト2020/b.cpp
@@ -13,6 +13,21 @@ vector<int>dys={0,1,0,-1};
 //fixed << setprecision(10)
 
 int main(){
-    
+    int a,b,m;
+    cin >> a >> b >> m;
+    vector<int>A(a),B(b);
+    rep(i,a)cin >> A[i];
+    rep(i,b)cin >> B[i];
+    int minA=*min_element(ALL(A));
+    int minB=*min_element(ALL(B));
+    int ans=minA+minB;
+
+    rep(i,m){
+        int x,y,c;
+        cin >> x >> y >> c;
+        x--,y--;
+        ans=min(ans,A[x]+B[y]-c);
+    }
+    cout << ans << endl;
     return 0;
 }
