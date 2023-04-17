2e40f74d49c412735f382a97635b4e14537f83cf
kaito <kaito1013kaito@gmail.com>
2023-04-17 09:54:18 +0900

    first commit

---
+++ Atcoder/other/天下一プログラマーコンテスト2012予選C/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int n;
+    cin >> n;
+    vector<bool>deleted(n+1);
+    for(int i=2;i<n;i++){
+        if(deleted[i])continue;
+        for(int j=i*2;j<n;j+=i)deleted[j]=true;
+    }
+    int ans=0;
+    for(int i=2;i<n;i++){
+        if(!deleted[i])ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
