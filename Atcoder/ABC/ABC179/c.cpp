4fe57fc3a039e68cdb0dabb98986a49e0143c753
kaito <kaito1013kaito@gmail.com>
2023-04-20 10:41:36 +0900

    first commit

--- Atcoder/ABC/ABC179/c.cpp
+++ Atcoder/ABC/ABC179/c.cpp
@@ -8,13 +8,9 @@ using ll = long long;
 int main(){
     int n;
     cin >> n;
-    int ans=0;
-    for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
-            for(int k=1;k<n;k++){
-                if(i*j+k==n)ans++;
-            }
-        }
+    ll ans=0;
+    for(int i=1;i<=n;i++){
+        ans+=(n-1)/i;
     }
     cout << ans << endl;
     return 0;
