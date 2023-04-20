490d6886551e4fbd43e126440c96cdae1f12f1af
kaito <kaito1013kaito@gmail.com>
2023-04-20 10:09:21 +0900

    first commit

---
+++ Atcoder/AGC/AGC024/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int a,b,c;
+    ll k;
+    cin >> a >> b >> c >> k;
+    if(k%2==0){
+        cout << a-b << endl;
+    }else{
+        cout << -a+b << endl;
+    }
+    return 0;
+}
