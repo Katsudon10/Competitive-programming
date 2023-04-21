5cd0d526f8368e2f0b1b43393995322e29a54925
kaito <kaito1013kaito@gmail.com>
2023-04-21 09:17:13 +0900

    first commit

--- Atcoder/AGC/AGC003/a.cpp
+++ Atcoder/AGC/AGC003/a.cpp
@@ -13,6 +13,22 @@ vector<int>dys={0,1,0,-1};
 //fixed << setprecision(10)
 
 int main(){
-    
+    string s;
+    cin >> s;
+    bool N=false,S=false,W=false,E=false;
+    for(char c:s){
+        if(c=='N')N=true;
+        if(c=='S')S=true;
+        if(c=='W')W=true;
+        if(c=='E')E=true;
+    }
+
+    if((N && !S) || (!N && S)){
+        cout << "No" << endl;
+    }else if((W && !E) || (!W && E)){
+        cout << "No" << endl;
+    }else{
+        cout << "Yes" << endl;
+    }
     return 0;
 }
