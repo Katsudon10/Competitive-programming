ba600df7773de7f395104be3849a42ee7cc1db6a
kaito <kaito1013kaito@gmail.com>
2023-05-01 09:21:08 +0900

    first commit

--- Atcoder/ARC/ARC153/a.cpp
+++ Atcoder/ARC/ARC153/a.cpp
@@ -13,6 +13,25 @@ vector<int>dys={0,1,0,-1};
 //fixed << setprecision(10)
 
 int main(){
-    
+    int n;
+    cin >> n;
+    ll cnt=0;
+    for(int i=1;i<=9;i++){
+        for(int j=0;j<=9;j++){
+            for(int k=0;k<=9;k++){
+                for(int l=0;l<=9;l++){
+                    for(int m=0;m<=9;m++){
+                        for(int o=0;o<=9;o++){
+                            cnt++;
+                            if(cnt==n){
+                                cout << i << i << j << k << l << l << m << o << m <<endl;
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
