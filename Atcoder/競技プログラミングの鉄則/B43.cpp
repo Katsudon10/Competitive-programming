f534013ce91b0d142295d63e3188a6ea22177ca7
kaito <kaito1013kaito@gmail.com>
2023-05-17 08:57:31 +0900

    first commit

---
+++ Atcoder/競技プログラミングの鉄則/B43.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<int>A(m);
+    rep(i,m)cin >> A[i];
+    map<int,int>mp;
+    rep(i,m){
+        mp[A[i]-1]++;
+    }
+    rep(i,n){
+        cout << m-mp[i] << endl;
+    }
+    return 0;
+}
