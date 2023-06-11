d3389a54a664b71b747878f04e381e346aac0d2f
kaito <kaito1013kaito@gmail.com>
2023-06-11 23:22:46 +0900

    first commit

--- Atcoder/ABC/ABC301/c.cpp
+++ Atcoder/ABC/ABC301/c.cpp
@@ -12,7 +12,41 @@ vector<int>dys={0,1,0,-1};
 
 //fixed << setprecision(10)
 
+vector<int> count(string s){
+    vector<int>cnt(27);
+    for(char c:s){
+        if(c=='@')cnt[26]++;
+        else cnt[c-'a']++;
+    }
+    return cnt;
+}
+
 int main(){
-    
+    string s,t;
+    cin >> s >> t;
+    vector<int>cs=count(s);
+    vector<int>ct=count(t);
+    string A="atcoder";
+
+    bool flag=true;
+    vector<bool>inA(26);
+    for(char c:A)inA[c-'a']=true;
+
+    rep(i,26) if(!inA[i]){
+        if(cs[i]!=ct[i])flag=false;
+    }
+
+    rep(i,26) if(inA[i]){
+        if(cs[i]<ct[i])cs[26]-=ct[i]-cs[i];
+        else ct[26]-=cs[i]-ct[i];
+    }
+
+    if(cs[26]<0)flag=false;
+    if(ct[26]<0)flag=false;
+    if(flag){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     return 0;
 }
