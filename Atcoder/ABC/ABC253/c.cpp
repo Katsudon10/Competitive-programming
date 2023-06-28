693c9c992dc81fcbf5b601cd0a46b7021cac54cc
kaito <kaito1013kaito@gmail.com>
2023-06-28 10:08:08 +0900

    first commit

--- Atcoder/ABC/ABC253/c.cpp
+++ Atcoder/ABC/ABC253/c.cpp
@@ -4,6 +4,26 @@ using namespace std;
 const int inf = INT_MAX;
 
 int main(){
-    
+    int q;
+    cin >> q;
+    multiset<int>s;
+    rep(i,q){
+        int query;
+        cin >> query;
+        if(query==1){
+            int x;
+            cin >> x;
+            s.insert(x);
+        }else if(query==2){
+            int x,c;
+            cin >> x >> c;
+            rep(i,c){
+                if(s.find(x)!=s.end())s.erase(s.find(x));
+                else break;
+            }
+        }else{
+            cout << *s.rbegin() - *s.begin() << endl;
+        }
+    }
     return 0;
 }
