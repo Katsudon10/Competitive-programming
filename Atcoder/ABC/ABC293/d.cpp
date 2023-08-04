57aff928534844dad8687e91018ab5ae48ef369e
kaito <kaito1013kaito@gmail.com>
2023-08-04 23:25:00 +0900

    first commit

--- Atcoder/ABC/ABC293/d.cpp
+++ Atcoder/ABC/ABC293/d.cpp
@@ -11,8 +11,44 @@ vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
 //fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
-    
+    int n,m;
+    cin >> n >> m;
+    Graph G(n);
+    vector<int>deg(n);
+    rep(i,m){
+        int a,c;
+        char b,d;
+        cin >> a >> b >> c >> d;
+        a--;c--;
+        G[a].push_back(c);
+        G[c].push_back(a);
+        deg[a]++;deg[c]++;
+    }
+    int x=0,y=0;
+    vector<bool>used(n,false);
+    rep(i,n){
+        if(!used[i]){
+            queue<int>que;
+            used[i]=true;
+            que.push(i);
+            bool flag=true;
+            while(!que.empty()){
+                int q=que.front();que.pop();
+                if(deg[q]!=2)flag=false;
+                for(int v:G[q]){
+                    if(!used[v]){
+                        que.push(v);
+                        used[v]=true;
+                    }
+                }
+            }
+            if(flag)x++;
+            else y++;
+        }
+    }
+    cout << x << ' ' << y << endl;
     return 0;
 }
