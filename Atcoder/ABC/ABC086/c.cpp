49625f7b3f71b5708cf50fb1831367ba1412d4f5
kaito <kaito1013kaito@gmail.com>
2023-08-06 22:56:47 +0900

    first commit

--- Atcoder/ABC/ABC086/c.cpp
+++ Atcoder/ABC/ABC086/c.cpp
@@ -15,5 +15,25 @@ vector<int>dys={0,1,0,-1};
 
 int main(){
     int n;
+    cin >> n;
+    vector<int>t(n),x(n),y(n);
+    rep(i,n)cin >> t[i] >> x[i] >> y[i];
+    int pt=0,px=0,py=0;
+    rep(i,n){
+        int d=abs(x[i]-px)+abs(y[i]-py);
+        int dt=t[i]-pt;
+        if(dt<d){
+            cout << "No" << endl;
+            return 0;
+        }
+        if((dt-d)%2==1){
+            cout << "No" << endl;
+            return 0;
+        }
+        pt=t[i];
+        px=x[i];
+        py=y[i];
+    }
+    cout << "Yes" << endl;
     return 0;
 }
