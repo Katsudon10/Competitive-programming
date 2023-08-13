1c025775a8b69ce1a578f672a0adfaf1921dd786
kaito <kaito1013kaito@gmail.com>
2023-08-13 22:44:02 +0900

    first commit

--- Atcoder/ABC/ABC314/b.cpp
+++ Atcoder/ABC/ABC314/b.cpp
@@ -16,20 +16,33 @@ vector<int>dys={0,1,0,-1};
 int main(){
     int n;
     cin >> n;
-    vector<set<P>>st;
+    vector<vector<int>>A(n);
+    vector<int>C(n);
     rep(i,n){
-        int c;
-        cin >> c;
-        rep(j,c){
-            int a;
-            cin >> a;
-            st[a].insert(P(c,i+1));
+        cin >> C[i];
+        A[i]=vector<int>(C[i]);
+        rep(j,C[i]){
+            cin >> A[i][j];
         }
     }
     int x;
     cin >> x;
-    for(auto v:st[x]){
-        cout << v.second << endl;
+
+    vector<int>temp;
+    rep(i,n){
+        rep(j,C[i]){
+            if(A[i][j]==x)temp.push_back(i);
+        }
     }
+
+    int cmin=inf;
+    for(int v:temp)cmin=min(cmin,C[v]);
+
+    vector<int>ans;
+    for(int v:temp)if(C[v]==cmin)ans.push_back(v+1);
+
+    cout << ans.size() << endl;
+    for(int v:ans)cout << v << ' ';
+    cout << endl;
     return 0;
 }
