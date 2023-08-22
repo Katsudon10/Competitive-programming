62194ab77bdee89d90eb85ee9c7f9916e5b695e5
kaito <kaito1013kaito@gmail.com>
2023-08-22 16:36:57 +0900

    first commit

---
+++ Atcoder/ABC/ABC091/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+int main(){
+    int n;
+    cin >> n;
+    vector<string>s(n);
+    rep(i,n)cin >> s[i];
+    int m;
+    cin >> m;
+    vector<string>t(m);
+    rep(i,m)cin >> t[i];
+
+    map<string,int>smp,tmp;
+    rep(i,n)smp[s[i]]++;
+    rep(i,m)tmp[t[i]]++;
+
+    int ans=0;
+    for(auto it=smp.begin();it!=smp.end();it++){
+        ans=max(ans,it->second-tmp[it->first]);
+    }
+    cout << ans << endl;
+    return 0;
+}
