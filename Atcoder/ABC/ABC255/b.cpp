002dc690609071a295a89f578cd171283e37da90
kaito <kaito1013kaito@gmail.com>
2023-08-26 16:05:16 +0900

    first commit

--- Atcoder/ABC/ABC255/b.cpp
+++ Atcoder/ABC/ABC255/b.cpp
@@ -15,16 +15,23 @@ vector<int>dys={0,1,0,-1};
 int main(){
     int n,k;
     cin >> n >> k;
-    vector<int>A(n);
-    rep(i,n)cin >> A[i];
-    vector<pair<ll,ll>>p(n);
-    rep(i,n)cin >> p[i].first >> p[i].second;
-    ll dir=0;
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            dir=max(dir,(p[i].first-p[j].first)*(p[i].first-p[j].first)+(p[i].second-p[j].second)*(p[i].second-p[j].second));
-        }
+    vector<int>a(k);
+    rep(i,k){
+        cin >> a[i];
+        a[i]--;
     }
-    cout << fixed << setprecision(10) << sqrt(dir) << endl;
-    return 0;
+    vector<int>x(n),y(n);
+    rep(i,n)cin >> x[i] >> y[i];
+
+    double ans=0;
+    rep(i,n){
+        double d=1e18;
+        rep(j,k){
+            double dx=x[i]-x[a[j]];
+            double dy=y[i]-y[a[j]];
+            d=min(d,(double)dx*dx+dy*dy);
+        }
+        ans=max(ans,d);
+    }
+    cout << fixed << setprecision(10) << sqrt(ans) << endl;
 }
