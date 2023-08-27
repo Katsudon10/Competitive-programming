59e00e76b24ccad96ab052bda8398d7101b64d6d
kaito <kaito1013kaito@gmail.com>
2023-08-27 23:39:34 +0900

    first commit

---
+++ Atcoder/other/CodeQUEEN2023決勝/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+int main(){
+    string s;
+    char c;
+    cin >> s >> c;
+    string ans;
+    for(char sc:s){
+        if(sc==c){
+            ans+=sc;
+            ans+=sc;
+        }else ans+=sc;
+    }
+    cout << ans << endl;
+    return 0;
+}
