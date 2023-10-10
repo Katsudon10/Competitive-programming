a2d04bef3a8fe25df778db0efa130c7f8b16b246
kaito <kaito1013kaito@gmail.com>
2023-10-10 22:51:35 +0900

    first commit

--- Atcoder/ABC/ABC284/d.cpp
+++ Atcoder/ABC/ABC284/d.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
 const int inf = INT_MAX;
 using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
 //fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
     int t;
@@ -11,12 +19,19 @@ int main(){
     rep(i,t){
         ll n;
         cin >> n;
-        for(ll j=2;j*j<=n/2;j++){
-            if(n%(j*j)==0){
-                cout << j << ' ' << n/(j*j) << endl;
-                break;
+        ll p,q;
+        for(ll i=2;i*i*i<=n;i++){
+            if(n%i!=0)continue;
+            if(n%(i*i)==0){
+                p=i;
+                q=n/i/i;
+            }else{
+                q=i;
+                p=(ll)round(sqrt(n/i));
             }
+            break;
         }
+        cout << p << ' ' << q << endl;
     }
     return 0;
 }
