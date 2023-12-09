7cfba0fc506c5191891aab16ca09fc72534e91dd
kaito <kaito1013kaito@gmail.com>
2023-12-09 23:54:52 +0900

    first commit

---
+++ Atcoder/ABC/ABC046/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    ll ans=1;
+    rep(i,n){
+        if(i==0)ans*=k;
+        else ans*=k-1;
+    }
+    cout << ans << endl;
+    return 0;
+}
