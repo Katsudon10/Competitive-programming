a37ce8f632883e571acbd91dbdba29807f1e517b
kaito <kaito1013kaito@gmail.com>
2024-03-13 23:54:42 +0900

    first commit

--- Atcoder/ABC/ABC243/c.cpp
+++ Atcoder/ABC/ABC243/c.cpp
@@ -19,6 +19,22 @@ int main(){
     rep(i,n)cin >> x[i] >> y[i];
     string s;
     cin >> s;
-    
+    map<int,int>L,R;
+    rep(i,n){
+        if(s[i]=='L'){
+            if(!L.count(y[i]))L[y[i]]=x[i];
+            else L[y[i]]=max(L[y[i]],x[i]);
+        }else{
+            if(!R.count(y[i]))R[y[i]]=x[i];
+            else R[y[i]]=min(R[y[i]],x[i]);
+        }
+    }
+    bool ans=false;
+    for(auto p:R){
+        auto key=p.first;
+        if(L.count(key) && R[key]<L[key])ans=true;
+    }
+    if(ans)cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
