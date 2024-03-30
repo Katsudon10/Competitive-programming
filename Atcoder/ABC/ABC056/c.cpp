dd01c77ac4cc275b785a6e06e679ee8edda53cec
kaito <kaito1013kaito@gmail.com>
2024-03-30 17:40:26 +0900

    first commit

---
+++ Atcoder/ABC/ABC056/c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll w;
+    Edge(int to,ll w):to(to),w(w){}
+};
+using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+int main(){
+    int x;
+    cin >> x;
+    int t=0,i=0;
+    while(i<x){
+        t++;
+        i+=t;
+    }
+    cout << t << endl;
+    return 0;
+}
