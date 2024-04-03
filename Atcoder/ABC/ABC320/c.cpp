b46fa05445a80c2e969ecf97d11a247143844491
kaito <kaito1013kaito@gmail.com>
2024-04-03 17:48:13 +0900

    first commit

--- Atcoder/ABC/ABC320/c.cpp
+++ Atcoder/ABC/ABC320/c.cpp
@@ -14,6 +14,20 @@ vector<int>dys={0,1,0,-1};
 //A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
-    
+    int m;
+    cin >> m;
+    vector<string>s(3);
+    rep(i,3)cin >> s[i];
+    int ans=inf;
+    rep(i,3*m)rep(j,3*m)rep(k,3*m){
+        if(i==j)continue;
+        if(i==k)continue;
+        if(j==k)continue;
+        if(s[0][i%m]!=s[1][j%m])continue;
+        if(s[0][i%m]!=s[2][k%m])continue;
+        ans=min(ans,max({i,j,k}));
+    }
+    if(ans==inf)ans=-1;
+    cout << ans << endl;
     return 0;
 }
