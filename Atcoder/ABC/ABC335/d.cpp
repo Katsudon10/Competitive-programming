8fb4029b9331e0887d7040f83ad971dae1022914
kaito <kaito1013kaito@gmail.com>
2024-04-08 12:51:09 +0900

    first commit

--- Atcoder/ABC/ABC335/d.cpp
+++ Atcoder/ABC/ABC335/d.cpp
@@ -16,6 +16,34 @@ vector<int>dys={0,1,0,-1};
 int main(){
     int n;
     cin >> n;
-    vector<vector<int> x(n,vector<int>(n));
+    
+    vector<vector<int>> ans(n,vector<int>(n,-1));
+
+    int cur=1;
+    int i=0,j=0;
+
+    auto f = [&](int di,int dj){
+        ans[i][j]=cur;
+        cur++;
+        i+=di;
+        j+=dj;
+    };
+
+    for(int l=n-1;l>0;l-=2){
+        rep(k,l)f(0,1);
+        rep(k,l)f(1,0);
+        rep(k,l)f(0,-1);
+        rep(k,l-1)f(-1,0);
+        f(0,1);
+    }
+    
+    rep(i,n){
+        rep(j,n){
+            if(ans[i][j]==-1)cout << 'T';
+            else cout << ans[i][j];
+            cout << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
