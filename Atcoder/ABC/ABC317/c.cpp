7a8e9df25fc7f5cb51f152831e7c3bf3f0d0ebcc
kaito <kaito1013kaito@gmail.com>
2024-04-14 15:22:11 +0900

    first commit

--- Atcoder/ABC/ABC317/c.cpp
+++ Atcoder/ABC/ABC317/c.cpp
@@ -5,55 +5,74 @@ using namespace std;
 const int inf = INT_MAX;
 using ll = long long;
 using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll w;
+    Edge(int to,ll w):to(to),w(w){}
+};
 using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
 
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
 vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
 //fixed << setprecision(10)
 //A[i].erase(unique(ALL(A[i])),A[i].end());
+ll ans=-1;
+void dfs(WeightedGraph &G,int v,vector<bool> &seen,ll dist){
+    seen[v]=true;
+    ans=max(ans,dist);
+    for(auto vs:G[v]){
+        if(seen[vs.to])continue;
+        dfs(G,vs.to,seen,dist+vs.w);
+    }
+    seen[v]=false;
+}
 
 int main(){
     int n,m;
     cin >> n >> m;
-    vector<vector<P>>G(n);
+    WeightedGraph G(n);
     rep(i,m){
         int a,b,c;
         cin >> a >> b >> c;
         a--;b--;
-        G[a].push_back(make_pair(b,c));
-        G[b].push_back(make_pair(a,c));
+        G[a].emplace_back(b,c);
+        G[b].emplace_back(a,c);
     }
 
-    ll ans=0;
     rep(i,n){
         vector<bool>seen(n,false);
-        seen[i]=true;
-        queue<P>que;
-        if(G[i].size()==0)continue;
-        que.push(G[i].front());
-        vector<ll>dist(n,-inf);
-        
-        dist[i]=0;
-        queue<int>posp;
-        posp.push(i);
-        while(!que.empty()){
-            int pos=que.front().first;
-            que.pop();
-            int pos2=posp.front();
-            posp.pop();
-            if(seen[pos])continue;
-            seen[pos]=true;
-            dist[pos]=max(dist[pos],dist[pos2]+que.front().second);
-            rep(j,G[pos].size()){
-                que.push(G[pos][j]);
-                posp.push(pos);
-            }
-        }
-        sort(dist.rbegin(),dist.rend());
-        ans=max(ans,dist[0]);
+        dfs(G,i,seen,0);
     }
-    
     cout << ans << endl;
     return 0;
 }
