df661a2188b101e703afdcd50fad72e2b2e31d2d
Kaito.I <kaito1013kaito@gmail.com>
2024-04-16 13:29:22 +0900

    first commit

--- Atcoder/ABC/ABC292/d.cpp
+++ Atcoder/ABC/ABC292/d.cpp
@@ -5,48 +5,65 @@ using namespace std;
 const int inf = INT_MAX;
 using ll = long long;
 using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll w;
+    Edge(int to,ll w):to(to),w(w){}
+};
 using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
 
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
 vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
 //fixed << setprecision(10)
-
-void rec(int v,vector<bool> &seen,vector<vector<int>> &G,int &num,int &vn){
-    seen[v]=true;
-    vn++;
-    for(int v2:G[v]){
-        num++;
-        if(seen[v2])continue;
-        rec(v2,seen,G,num,vn);
-    }
-}
+//A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
     int n,m;
     cin >> n >> m;
+    UnionFind uf(n);
 
-    Graph G(n);
-    vector<bool>seen(n,false);
-
+    vector<int>u(m),v(m);
     rep(i,m){
-        int u,v;
-        cin >> u >> v;
-        u--;v--;
-        G[u].push_back(v);
-        G[v].push_back(u);
+        cin >> u[i] >> v[i];
+        u[i]--,v[i]--;
+        uf.unite(u[i],v[i]);
     }
 
-    rep(i,n){
-        int num=0,vn=0;
-        if(seen[i])continue;
-        rec(i,seen,G,num,vn);
-        //cout << num/2 << ' ' << vn << endl;
-        if(vn!=num/2){
-            cout << "No" << endl;
-            return 0;
-        }
-    }
-    cout << "Yes" << endl;
+    vector<int>cntN(n,0),cntE(n,0);
+    rep(i,n)cntN[uf.root(i)]++;
+    rep(i,m)cntE[uf.root(u[i])]++;
+    if(cntN==cntE)cout << "Yes" << endl;
+    else cout << "No" << endl;
+
     return 0;
 }
