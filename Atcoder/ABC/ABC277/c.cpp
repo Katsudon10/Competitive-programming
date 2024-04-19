3648d9a87d7f5f82bdf936dfa45cc26cb8ca54e5
kaito <kaito1013kaito@gmail.com>
2024-04-19 11:18:57 +0900

    first commit

--- Atcoder/ABC/ABC277/c.cpp
+++ Atcoder/ABC/ABC277/c.cpp
@@ -1,12 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
 const int inf = INT_MAX;
 using ll = long long;
-//fixed << setprecision(5)
+using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll w;
+    Edge(int to,ll w):to(to),w(w){}
+};
+using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
+
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+void dfs(map<int,vector<int>> &G,int v,set<int> &st){
+    st.insert(v);
+    for(int vs:G[v]){
+        if(st.count(vs))continue;
+        dfs(G,vs,st);
+    }
+}
 
 int main(){
     int n;
+    cin >> n;
+    map<int,vector<int>> G;
     
+    rep(i,n){
+        int a,b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    set<int>st;
+    dfs(G,1,st);
+    cout << *st.rbegin() << endl;
     return 0;
 }
