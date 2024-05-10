83e1069887aa826c69e5921b39cae46bcefd99f7
kaito <kaito1013kaito@gmail.com>
2024-05-10 16:59:11 +0900

    first commit

---
+++ Atcoder/競プロ典型90/044.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+const ll INF = 1e18;
+using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll cost;
+    Edge(int to,ll cost):to(to),cost(cost){}
+};
+using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
+
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+int main(){
+    int n,q;
+    cin >> n >> q;
+    deque<int>deq;
+    rep(i,n){
+        int a;
+        cin >> a;
+        deq.push_back(a);
+    }
+
+    rep(i,q){
+        int t,x,y;
+        cin >> t >> x >> y;
+        x--,y--;
+        if(t==1){
+            swap(deq[x],deq[y]);
+        }else if(t==2){
+            deq.push_front(deq.back());
+            deq.pop_back();
+        }else{
+            cout << deq[x] << endl;
+        }
+    }
+    return 0;
+}
