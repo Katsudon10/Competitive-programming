ed37f6d2605536b7439798c796e8e91d4dad2ee9
kaito <kaito1013kaito@gmail.com>
2024-05-24 11:37:17 +0900

    first commit

--- Atcoder/ABC/ABC278/d.cpp
+++ Atcoder/ABC/ABC278/d.cpp
@@ -4,15 +4,78 @@ using namespace std;
 #define ALL(a)  (a).begin(),(a).end()
 const int inf = INT_MAX;
 using ll = long long;
+const ll INF = 1e18;
 using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll cost;
+    Edge(int to,ll cost):to(to),cost(cost){}
+};
 using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
 
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
 vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
 //fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
-
+    int n,q;
+    cin >> n;
+    map<int,ll>mp;
+    rep(i,n){
+        int a;
+        cin >> a;
+        mp[i+1]+=a;
+    }
+    cin >> q;
+    ll cur=0;
+    rep(qi,q){
+        int t;
+        cin >> t;
+        if(t==1){
+            int x;
+            cin >> x;
+            cur=x;
+            mp=map<int,ll>();
+        }else if(t==2){
+            int i,x;
+            cin >> i >> x;
+            mp[i]+=x;
+        }else{
+            int i;
+            cin >> i;
+            cout << cur+mp[i] << endl;
+        }
+    }
     return 0;
 }
