9604cd959664882b8079f2540310069ff741066b
kaito <kaito1013kaito@gmail.com>
2024-05-28 23:43:39 +0900

    first commit

--- Atcoder/other/ChokudaiSpeedRun001/f.cpp
+++ Atcoder/other/ChokudaiSpeedRun001/f.cpp
@@ -4,9 +4,44 @@ using namespace std;
 #define ALL(a)  (a).begin(),(a).end()
 const int inf = INT_MAX;
 using ll = long long;
+const ll INF = 1e18;
 using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll cost;
+    Edge(int to,ll cost):to(to),cost(cost){}
+};
 using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
 
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
 vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
@@ -16,15 +51,14 @@ vector<int>dys={0,1,0,-1};
 int main(){
     int n;
     cin >> n;
-    vector<int>a(n);
-    rep(i,n)cin >> a[i];
-    ll ans=0;
-    for(int i=0;i<n;i++){
-        bool flag=true;
-        for(int j=0;j<i;j++){
-            if(a[i]<=a[j])flag=false;
+    int ans=0,mx=0;
+    rep(i,n){
+        int a;
+        cin >> a;
+        if(mx<a){
+            ans++;
+            mx=a;
         }
-        if(flag)ans++;
     }
     cout << ans << endl;
     return 0;
