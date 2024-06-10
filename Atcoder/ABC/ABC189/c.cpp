37d78c911ab0f37ccb63033b7c54366b01586416
kaito <kaito1013kaito@gmail.com>
2024-06-10 17:12:12 +0900

    first commit

--- Atcoder/ABC/ABC189/c.cpp
+++ Atcoder/ABC/ABC189/c.cpp
@@ -1,34 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
 const int inf = INT_MAX;
 using ll = long long;
-//fixed << setprecision(5)
+const ll INF = 1e18;
+using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll cost;
+    Edge(int to,ll cost):to(to),cost(cost){}
+};
+using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
+
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
 
 int main(){
-    int n,m,k;
-    cin >> n >> m;
-    vector<int>a(m),b(m);
-    rep(i,m)cin >> a[i] >> b[i];
-    cin >> k;
-    vector<int>c(k),d(k);
-    rep(i,k) cin >> c[i] >> d[i];
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    rep(i,n)cin >> a[i];
+
     int ans=0;
-    for(int bit=0;bit<(1<<k);bit++){
-        vector<bool>ball(n);
-        int tmp=0;
-        for(int i=0;i<k;i++){
-            if(bit & (1<<i)){
-                ball[c[i]]=true;
-            }else{
-                ball[d[i]]=true;
-            }
+    rep(i,n){
+        int cnt=a[i];
+        for(int j=i;j<n;j++){
+            cnt=min(cnt,a[j]);
+            ans=max(ans,cnt*(j-i+1));
         }
-        for(int j=0;j<m;j++){
-            if(ball[a[j]] && ball[b[j]]) tmp++;
-        }
-        if(tmp>ans) swap(tmp,ans);
     }
-    cout << ans << endl;  
+    cout << ans << endl;
     return 0;
 }
