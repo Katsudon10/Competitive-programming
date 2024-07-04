fc3afe13501a2bbcfc1755c595fd2eb457f48577
kaito <kaito1013kaito@gmail.com>
2024-07-04 22:51:56 +0900

    edit snippets

--- snippets.cpp
+++ snippets.cpp
@@ -42,6 +42,37 @@ struct UnionFind{
        return siz[root(x)];
    }
 };
+
+class SegmentTree{
+   public:
+   vector<int>dat;
+   int siz=1;
+
+   void init(int N){
+       siz=1;
+       while(siz<N)siz*=2;
+       dat=vector<int>(2*siz,0);
+   }
+
+   void update(int pos,int x){
+       pos=pos+siz-1;
+       dat[pos]=x;
+       while(pos>=2){
+           pos/=2;
+           dat[pos]=max(dat[pos*2],dat[pos*2+1]);
+       }
+   }
+
+   int query(int l,int r,int a,int b,int u){
+       if(r<=a || b<=l)return -100000000;
+       if(l<=a && b<=r)return dat[u];
+       int m=(a+b)/2;
+       int AnsL=query(l,r,a,m,u*2);
+       int AnsR=query(l,r,m,b,u*2+1);
+       return max(AnsL,AnsR);
+   }
+};
+
 vector<int>dxs={1,0,-1,0};
 vector<int>dys={0,1,0,-1};
 
