8ec7141c188f0bcff4cb1933ee09d29422f44f77
kaito <kaito1013kaito@gmail.com>
2024-10-07 17:06:04 +0900

    first commit

---
+++ Atcoder/EDPC/p.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+const int inf = INT_MAX;
+using ll = long long;
+const ll INF = 1e18;
+using P = pair<int,int>;
+struct Edge{
+    int to;
+    ll cost;
+    Edge(int to,ll cost):to(to),cost(cost){}
+};
+using Graph = vector<vector<int>>;
+using WeightedGraph = vector<vector<Edge>>;
+
+
+struct UnionFind{
+   vector<int> par,rank,siz;
+   UnionFind(int n) : par(n,-1),rank(n,0),siz(n,1){ }
+
+   int root(int x){
+       if(par[x]==-1)return x;
+       else return par[x]=root(par[x]);
+   }
+
+   bool issame(int x,int y){
+       return root(x)==root(y);
+   }
+
+   bool unite(int x,int y){
+       int rx=root(x),ry=root(y);
+       if(rx==ry)return false;
+       if(rank[rx]<rank[ry])swap(rx,ry);
+       par[ry]=rx;
+       if(rank[rx]==rank[ry])rank[rx]++;
+       siz[rx]+=siz[ry];
+       return true;
+   }
+
+   int size(int x){
+       return siz[root(x)];
+   }
+};
+
+class SegmentTree{
+   public:
+   vector<int>dat;
+   int siz=1;
+
+   void init(int N){
+       siz=1;
+       while(siz<N)siz*=2;
+       dat=vector<int>(2*siz,0);
+   }
+
+   void update(int pos,int x){
+       pos=pos+siz-1;
+       dat[pos]=x;
+       while(pos>=2){
+           pos/=2;
+           dat[pos]=max(dat[pos*2],dat[pos*2+1]);
+       }
+   }
+
+   int query(int l,int r,int a,int b,int u){
+       if(r<=a || b<=l)return -100000000;
+       if(l<=a && b<=r)return dat[u];
+       int m=(a+b)/2;
+       int AnsL=query(l,r,a,m,u*2);
+       int AnsR=query(l,r,m,b,u*2+1);
+       return max(AnsL,AnsR);
+   }
+};
+
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+
+vector<int>dxs={1,0,-1,0};
+vector<int>dys={0,1,0,-1};
+
+//fixed << setprecision(10)
+//A[i].erase(unique(ALL(A[i])),A[i].end());
+
+const int mod=1e9+7;
+
+void dfs(const Graph &g,vector<vector<ll>> &dp,int v,vector<bool>&seen,int p=-1){
+    seen[v]=true;
+    dp[v][0]=dp[v][1]=1;
+    for(int vs:g[v]){
+        if(vs==p)continue;
+        if(!seen[vs])dfs(g,dp,vs,seen,v);
+        dp[v][0]=(dp[v][0]*(dp[vs][0]+dp[vs][1])%mod)%mod;
+        dp[v][1]=dp[v][1]*dp[vs][0]%mod;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    Graph g(n);
+
+    rep(i,n-1){
+        int x,y;
+        cin >> x >> y;
+        x--,y--;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+
+    vector<vector<ll>>dp(n,vector<ll>(2,0));
+    vector<bool>seen(n,false);
+    dfs(g,dp,0,seen);
+    cout << (dp[0][0]+dp[0][1])%mod << endl;
+    return 0;
+}
