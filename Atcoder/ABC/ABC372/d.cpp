13a8e13d046f9de1b69c8a62bf1607db239b7130
kaito <kaito1013kaito@gmail.com>
2024-11-01 17:18:09 +0900

    first commit

--- Atcoder/ABC/ABC372/d.cpp
+++ Atcoder/ABC/ABC372/d.cpp
@@ -87,11 +87,15 @@ int main(){
     cin >> n;
     vector<int>h(n);
     rep(i,n)cin >> h[i];
+
     vector<int>ans(n);
-    rep(i,n){
-        
+    vector<int>st;
+    for(int i=n-1;i>=0;i--){
+        ans[i]=st.size();
+        while(st.size()>0 && st.back()<h[i])st.pop_back();
+        st.push_back(h[i]);
     }
-    for(int v:ans)cout << v << ' ';
+    rep(i,n)cout << ans[i] << ' ';
     cout << endl;
     return 0;
 }
